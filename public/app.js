ccf80369842a19304223a705626e8ccb98e41ae7
Evan Clevenger <evanclevenger@gmail.com>
2023-04-06 19:57:23 -0500

    added bootstrap css framework

--- public/app.js
+++ public/app.js
@@ -1,10 +1,11 @@
 //this is the only function in this folder
-//this  function calls the API
+//this  file and function calls the API
 
 const LoadRandomUsername = (resultDiv) => { //the only thing passed into this function is a referance to the place that we want to populate the results
     fetch('http://localhost:3001/animal')
     .then(response => response.json()) //converts the response, from the fetch, into a javascript object
     .then(result => {
+        resultDiv.classList.add('alert', 'alert-success');
         resultDiv.innerHTML = `<h2>${result.animal} ${result.number}<h2>`;
     }) //populates the result div as an argument 
 }
